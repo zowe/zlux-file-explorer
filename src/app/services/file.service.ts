7db8b68d4e1f0e5778f7d0523f1f1b629c691490
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-06-29 09:23:21 -0400

    add search history service
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -12,9 +12,10 @@
 
 import { Injectable } from '@angular/core';
 import { Http} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class FileService {
