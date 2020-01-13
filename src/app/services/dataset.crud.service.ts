050d936155dfca45c041747a73486f284ea1d6fe
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-01-13 14:58:08 -0500

    Remove unneeded edge case
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- src/app/services/dataset.crud.service.ts
+++ src/app/services/dataset.crud.service.ts
@@ -81,12 +81,12 @@ export class DatasetCrudService {
 
   queryDatasets(query:string, detail?: boolean, includeAdditionalQualifiers?: boolean): Observable<any>  {
     let url:string;
-    if (!query.includes('.')){
-      url = ZoweZLUX.uriBroker.datasetMetadataUri(query.toUpperCase( ) + '*');
-    }
-    else{
+    // if (!query.includes('.')){
+    //   url = ZoweZLUX.uriBroker.datasetMetadataUri(query.toUpperCase( ) + '*');
+    // }
+    // else{
       url = ZoweZLUX.uriBroker.datasetMetadataUri(query.toUpperCase( ).replace(/\.$/, ''), detail.toString(), undefined, true, undefined, undefined, undefined, undefined, undefined, includeAdditionalQualifiers.toString());
-    }
+    //}
     return this.http.get(url)
     .map(res=>res.json())
     .catch(this.handleErrorObservable);
