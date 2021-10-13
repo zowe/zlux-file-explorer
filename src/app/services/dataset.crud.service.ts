27fdbbca6db191e4146a956d88e338482a5a0712
Adarshdeep Cheema <adarshdeep.cheema@ibm.com>
2021-10-13 14:53:39 -0400

    fixing the defect where we cannot search for DS members under PDS, in Dataset TRee, if the PDS name contains special characters
    Signed-off-by: Adarshdeep Cheema <adarshdeep.cheema@ibm.com>

--- src/app/services/dataset.crud.service.ts
+++ src/app/services/dataset.crud.service.ts
@@ -88,7 +88,7 @@ export class DatasetCrudService {
 
   queryDatasets(query:string, detail?: boolean, includeAdditionalQualifiers?: boolean): Observable<any>  {
     let url:string;
-    url = ZoweZLUX.uriBroker.datasetMetadataUri(query.toUpperCase( ).replace(/\.$/, ''), detail.toString(), undefined, true, undefined, undefined, undefined, undefined, undefined, includeAdditionalQualifiers.toString());
+    url = ZoweZLUX.uriBroker.datasetMetadataUri(encodeURIComponent(query.toUpperCase( ).replace(/\.$/, '')), detail.toString(), undefined, true, undefined, undefined, undefined, undefined, undefined, includeAdditionalQualifiers.toString());
     return this.http.get(url)
     .map(res=>res.json())
     .catch(this.handleErrorObservable);
