3764db5cc089b046e294d1035d2a38b701602ce1
Adarshdeep Cheema <adarshdeep.cheema@ibm.com>
2022-06-30 08:24:55 -0400

    removing the tab on the clear icon
    Signed-off-by: Adarshdeep Cheema <adarshdeep.cheema@ibm.com>

--- src/app/components/create-file-modal/create-file-modal.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from '@angular/core';
-export declare class CreateFileModal {
-    private fileName;
-    private dirPath;
-    private filePathObtainedFromNode;
-    onFileCreate: EventEmitter<any>;
-    constructor(data: any);
-    createFile(): void;
-}
