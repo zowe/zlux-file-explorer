39f0e64110868368887ba0c874a99a732f197e7f
Sakshi Bobade <bobadesakshi21@gmail.com>
2023-07-21 15:27:35 +0530

    Revert "Restoring"
    This reverts commit 71fc8d58eaf6d1c125fac8d53027b4d23024ce01.

--- zlux-file-explorer/src/app/shared/file-tag.d.ts
+++
@@ -1,7 +0,0 @@
-export interface FileTag {
-    name: string;
-    codeset: number;
-    type: ZLUX.TagType;
-}
-export declare function findFileTagByCodeset(codeset: number): FileTag | undefined;
-export declare const fileTagList: FileTag[];
