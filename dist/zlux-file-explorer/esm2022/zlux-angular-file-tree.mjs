da3c445448d22890b8306437a06b08234cf23d5a
Satish Wadkar <swadkar@rocketsoftware.com>
2024-10-17 17:58:30 +0530

    remove mock data
    Signed-off-by: Satish Wadkar <swadkar@rocketsoftware.com>
    Signed-off-by: Sakshi Bobade <sbobade@rocketsoftware.com>

---
+++ dist/zlux-file-explorer/esm2022/zlux-angular-file-tree.mjs
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public-api';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiemx1eC1hbmd1bGFyLWZpbGUtdHJlZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Byb2plY3RzL3psdXgtZmlsZS1leHBsb3Jlci9zcmMvemx1eC1hbmd1bGFyLWZpbGUtdHJlZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7R0FFRztBQUVILGNBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL3B1YmxpYy1hcGknO1xuIl19
--- dist/zlux-file-explorer/index.d.ts
+++ dist/zlux-file-explorer/index.d.ts
@@ -1,5 +1,5 @@
 /**
  * Generated bundle index. Do not edit.
  */
-/// <amd-module name="@zowe/zlux-angular-file-tree" />
+/// <amd-module name="zlux-angular-file-tree" />
 export * from './public-api';
