bcc07dd6d42ebb716069535b6a45303948dd972b
Sakshi Bobade <sbobade@rocketsoftware.com>
2025-06-09 18:00:15 +0530

    Remove dist folder
    Signed-off-by: Sakshi Bobade <sbobade@rocketsoftware.com>

--- dist/zlux-file-explorer/lib/shared/error-state-matcher.d.ts
+++
@@ -1,5 +0,0 @@
-import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-export declare class CustomErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean;
-}
