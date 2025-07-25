9994e5bdbf421eac1eaab4dff13a6a2330d5217d
Sakshi Bobade <bobadesakshi21@gmail.com>
2025-07-25 16:52:50 +0530

    Detecting a double click
    Signed-off-by: Sakshi Bobade <bobadesakshi21@gmail.com>

--- projects/zlux-file-explorer/src/lib/components/tree/tree.component.ts
+++ projects/zlux-file-explorer/src/lib/components/tree/tree.component.ts
@@ -49,28 +49,57 @@ export class TreeComponent implements AfterContentInit, OnDestroy {
   @Output() rightClickEvent = new EventEmitter<MouseEvent>();
   @Output() panelRightClickEvent = new EventEmitter<MouseEvent>();
   selectedNode: FileNode;
-  lastClickedNodeName: string; // PrimeNG as of 6.0 has no native double click support for its tree
-  lastClickedNodeTimeout: number = 500; // < 500 ms becomes a double click
+  doubleClickTimer : number = 300; // Interval of 300ms or less between two clicks is considered a double-click here.
+  clickTimer: any = null;
   @ViewChild('fileExplorerPTree', { static: true }) fileExplorerTree: ElementRef;
-  constructor() {
-    this.lastClickedNodeName = null;
-  }
 
   /**
    * [nodeSelect provides the child folder click event to the parent file/folder tree tab]
    * @param  _event [click event]
    * @return        [void]
    */
+
+  // PrimeNG as of 17.18.10 has no native double-click support for p-tress, so we detect it using time interval between two clicks.
+  // After each select or unselect event, start a timer.
+  // If user clicks the same node before timer expires, treat it as a double-click & clear the timer. Else, handle the original select/unselect event.
+
   nodeSelect(_event?: any) {
-    console.log('tree-component: single click');
+    console.log('tree-component: node select');
     if (_event) {
-      this.clickEvent.emit(_event);
+      if (this.clickTimer) {
+        this.onNodeDblClick(_event);
+      } else {
+        this.clickTimer = setTimeout(() => {
+          this.onNodeSingleClick(_event);
+          this.clickTimer = null;
+        }, this.doubleClickTimer);
+      }
     }
   }
 
-  onNodeDblClick(event: any) {
+  nodeUnSelect(_event?: any) {
+    if(_event) {
+      if (this.clickTimer) {
+        this.onNodeDblClick(_event);
+      } else {
+        this.clickTimer = setTimeout(() => {
+          console.log('node unselected---', this.clickTimer);
+          this.clickTimer = null;
+        }, this.doubleClickTimer);
+      }
+    }
+  }
+
+  onNodeSingleClick(_event: any) {
+    console.log('tree-component: single click');
+    this.clickEvent.emit(_event);
+  }
+
+  onNodeDblClick(_event: any) {
     console.log('tree-component: double click');
-    this.dblClickEvent.emit(event);
+    this.dblClickEvent.emit(_event);
+    clearTimeout(this.clickTimer);
+    this.clickTimer = null;
   }
 
   nodeRightClickSelect(_event?: any) {
